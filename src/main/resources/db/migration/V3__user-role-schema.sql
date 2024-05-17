79ca27585a91639c3c6267d9a3d58b728aecaa74
DEV4Sep <mr.yisivlay@gmail.com>
2024-05-17 15:08:06 +0700

    updated flyway schema.

--- src/main/resources/db/migration/V3__user-role-schema.sql
+++ src/main/resources/db/migration/V3__user-role-schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE tbl_user_role (
-  user_id bigint NOT NULL,
-  role_id bigint NOT NULL,
-  PRIMARY KEY (user_id,role_id),
-  CONSTRAINT fk_tbl_role FOREIGN KEY (role_id) REFERENCES tbl_role(id),
-  CONSTRAINT fk_tbl_user FOREIGN KEY (user_id) REFERENCES tbl_user(id)
+CREATE TABLE tbl_user_roles (
+  users_id bigint NOT NULL,
+  roles_id bigint NOT NULL,
+  PRIMARY KEY (users_id,roles_id),
+  CONSTRAINT fk_tbl_role FOREIGN KEY (roles_id) REFERENCES tbl_role(id),
+  CONSTRAINT fk_tbl_user FOREIGN KEY (users_id) REFERENCES tbl_user(id)
 );
